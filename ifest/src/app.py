eef9e6e33f97b580245ae49eb1aa3b604a571c7d
alexia santos <alexiakarine18@mail.com>
2023-04-18 22:42:34 -0300

    cors

--- ifest/src/app.py
+++ ifest/src/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from routes.blueprint import blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Registering the blueprint
 app.register_blueprint(blueprint, url_prefix='/chat')
 
@@ -10,3 +11,4 @@ app.register_blueprint(blueprint, url_prefix='/chat')
 def main():
     return 'Olá! Bem-vindo ao iFest. Qual o seu nome?'
 
+app.run()
