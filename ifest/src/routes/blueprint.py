435cbfc267516576aa6fd658bd8dcebf845228e6
Guilherme Perfeito <guilherme.perfeito@vitaebrasil.com.br>
2023-04-20 20:18:18 -0300

    Convencional

--- ifest/src/routes/blueprint.py
+++ ifest/src/routes/blueprint.py
@@ -11,12 +11,19 @@ def receber():
     mensagem = request.json['mensagem']
     contexto = request.json['contexto']
     n =  request.json['n']
+    imagens_url = []
+    
+    images_path = [
+        'C:\Projects\Faculdade\ChatterBotIfest\ifest\images\chacara.jpeg',
+        'C:\Projects\Faculdade\ChatterBotIfest\ifest\images\salao.jpeg'
+    ]
+    imagens_url = [f'http://localhost:5000/{p}' for p in images_path]
 
     if contexto == "finalizar":
         return finalizar()
     else:
         resposta, contexto, n = respostas(mensagem, contexto, n)
-        return {'resposta': resposta, 'contexto': contexto, "n": n}
+        return {'resposta': resposta, 'contexto': contexto, "n": n, "imagem":imagens_url}
 
 
 blueprint.route('/teste', methods=['GET'])(testeChat)
