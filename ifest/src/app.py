f3289f35d593661bd4e3f03fbe3297395382b0fb
Guilherme Perfeito <guilherme.perfeito@vitaebrasil.com.br>
2023-04-20 18:51:11 -0300

    convencional

--- ifest/src/app.py
+++ ifest/src/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from routes.blueprint import blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Registering the blueprint
 app.register_blueprint(blueprint, url_prefix='/chat')
 
